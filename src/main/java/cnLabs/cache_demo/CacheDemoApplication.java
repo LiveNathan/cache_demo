16792276087a50c60e4aa41613ef05f28a4a7949
Nathan Lively <60303904+LiveNathan@users.noreply.github.com>
2023-03-01 19:28:28 -0600

    caching is working

--- src/main/java/cnLabs/cache_demo/CacheDemoApplication.java
+++ src/main/java/cnLabs/cache_demo/CacheDemoApplication.java
@@ -2,7 +2,9 @@ package cnLabs.cache_demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
+@EnableCaching
 @SpringBootApplication
 public class CacheDemoApplication {
 
